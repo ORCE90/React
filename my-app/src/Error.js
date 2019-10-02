f29562b256c64a3369e0f24e64b0c18661ffd061
Orce <apostolovski.orce@hotmail.com>
2019-10-02 21:49:31 +0200

    6

---
+++ my-app/src/Error.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Error = () => {
+
+    return <div> 
+        Oops, something went wrong.
+        Try going back or refreshing the page.
+        <button id='go back' onClick={() => window.history.back()}>  
+            Go back
+        </button>
+    </div>
+}
+
+export default Error 
--- my-app/src/Wrapper.js
+++ my-app/src/Wrapper.js
@@ -1,20 +1,59 @@
 import React from 'react'
 import User from './User'
-import data from './data.js'
+import Error from './Error'
+import axios from 'axios'
 
 
-const Wrapper = (props) => {
-    let users = []
-    users = data.map(element => { 
-        return <User key = {element.id} name={element.name} email={element.email} />
-    })
+
+
+class Wrapper extends React.Component  {
+    // let users = []
+    // users = data.map(element => { 
+    //     return <User key = {element.id} name={element.name} email={element.email} />
+    // })
+    constructor (props) {
+        super(props)
+        this.state = {
+            users: [], 
+            data: null
+        
+        }
+    }
+
+    componentDidMount () {
+            axios.get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => {
+                const users = response.data.map((element) => {
+                    return <User 
+                    key = {element.id} 
+                    name={element.name} 
+                    email={element.email} 
+                    />
+                })
+
+                this.setState({ data: users})
+            })
+
+            .catch((error) => {
+                this.setState({ data: <Error /> })
+            })
+    }
+
+    render () {
         return (
             <React.Fragment>
-                {props.children}
-                {users}
+                {this.props.children}
+                {
+                    ! this.state.data
+                    ? <h6> LOADING ... </h6>
+                    : this.state.data
+               
+                }
+                {this.state.error}
             </React.Fragment>
             
         )
+    }
 }
 
 export default Wrapper
